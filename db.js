2f8f1102f2d3c12a41a7bcb8b8ed8845198337b0
Kimberly Long <kimberlylong2262@gmail.com>
2022-03-24 14:57:18 -0400

    changed token back to original

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
-const sequelize = new Sequelize(process.env.DATABASE_URL || `postgresql://postgres:${encodeURIComponent(process.env.PASS)}@localhost:5432/AngerManagement`, {
-    dialect: 'postgres',
-    ssl: process.env.ENVIRONMENT === 'production'
-})
+// const sequelize = new Sequelize(process.env.DATABASE_URL || `postgresql://postgres:${encodeURIComponent(process.env.PASS)}@localhost:5432/AngerManagement`, {
+//     dialect: 'postgres',
+//     ssl: process.env.ENVIRONMENT === 'production'
+// })
 
-// const { Sequelize } = require("sequelize");
-// const db = new Sequelize(process.env.DB_CONNECTION_STRING);
+const { Sequelize } = require("sequelize");
+const db = new Sequelize(process.env.DB_CONNECTION_STRING);
 
 
 module.exports=db;
--- middleware/validate-session.js
+++ middleware/validate-session.js
@@ -1,47 +1,38 @@
 const jwt = require("jsonwebtoken")
 const { models } = require("../model")
-
 const validateJWT = async (req, res, next) => {
-    try{
+  if (req.method == "OPTIONS") {
+    next();
+  } else if (
+    req.headers.authorization &&
+    req.headers.authorization.includes("Bearer")
+  ) {
+    const { authorization } = req.headers
+    // console.log("authorization -->,", authorization)
+    const payload = authorization
+      ? jwt.verify(
+        authorization.includes("Bearer")
+          ? authorization.split(" ")[1]
+          : authorization,
+          process.env.JWT_SECRET
+      )
+      : undefined;
+      // console.log('payload -->", payload')
+      if (payload) {
+        let foundUser = await models.UserModel.findOne({ where: { id: payload.id } })
+        // console.log("foundUser -->", foundUser)
+        if (foundUser) {
+          req.user = foundUser;
+          next()
 
-        if (req.method == "OPTIONS") {
-            next();
-        } else if (
-            req.headers.authorization &&
-            req.headers.authorization.includes('Bearer')
-
-            ) {
-                const { authorization } = req.headers;
-                // console.log("Authorization", authorization);
-            const payload = authorization ? jwt.verify(
-                authorization,
-                process.env.JWT_SECRET
-        ) : undefined;
-        
-        console.log("Payload:", payload);
-
-        if (payload) {
-            let foundUser = await models.UserModel.findOne({ where: { id: payload.id } });
-            
-            // console.log("Found user:", foundUser);
-            
-            if (foundUser) {
-                // console.log("Request:", req);
-                req.user = foundUser;
-                next();
-            } else {
-                res.status(400).send({ message: "Not authorized" })
-            }
         } else {
-            res.status(401).send({ message: "Invalid token" })
+          res.status(400).send({ message: "Not Authorized" })
         }
-    } else {
-        res.status(403).send({ message: "Forbidden" })
-    }
-}catch(err){
-    res.status(500).send({ error: err })
-
+      } else {
+        res.status(401).send({ message: "INvalid token" })
+      }
+  } else {
+    res.status(403).send({ message: "Forbidden" })
+  }
 }
-}
-
-module.exports = validateJWT;
+module.exports = validateJWT
